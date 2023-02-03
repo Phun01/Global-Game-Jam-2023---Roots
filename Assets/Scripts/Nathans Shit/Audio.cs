38aad9012a6b69e28d2f2d5a05110a7a3a1d5045
NathanBorona <39974058+NathanBorona@users.noreply.github.com>
2023-02-04 00:07:44 +0800

    nates push

---
+++ Assets/Scripts/Nathans Shit/Audio.cs
@@ -0,0 +1,263 @@
+﻿using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    [Header("Tencion")]
+    public int tenceNo;
+    public AudioSource drums;
+    public AudioSource piano;
+    public float rollUpSpeed;
+    public float coolOffTime;
+    float coolOffTimer;
+    bool coolOff;
+
+    [Header("Player")]
+    public AudioSource[] footSteps;
+    int footStepNo;
+    bool footStepExists;
+
+    public AudioSource[] pistol;
+    int pistolNo;
+    bool pistolExists;
+
+    public AudioSource[] ricoshate;
+    int ricoshateNo;
+    bool ricoshateExists;
+
+    public AudioSource[] playerHey;
+    int playerHeyNo;
+    bool playerHeyExists;
+
+    public AudioSource[] playerHurt;
+    int playerHurtNo;
+    bool playerHurtExists;
+
+    public AudioSource[] playerCelebrate;
+    int playerCelebrateNo;
+    bool playerCelebrateExists;
+
+    [Header("Enemy")]
+    public AudioSource[] enemyHurt;
+    int enemyHurtNo;
+    bool enemyHurtExists;
+
+    public AudioSource[] enemyAttack;
+    int enemyAttackNo;
+    bool enemyAttackExists;
+
+    public AudioSource[] enemyDie;
+    int enemyDieNo;
+    bool enemyDieExists;
+
+    public AudioSource[] enemyCharge;
+    int enemyChargeNo;
+    bool enemyChargeExists;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(tenceNo == 1 && piano.volume < 1)
+        {
+            coolOff = true;
+            piano.volume += Time.deltaTime * rollUpSpeed;
+        }
+        else if (tenceNo != 1 && piano.volume > 0)
+        {
+            piano.volume -= Time.deltaTime * rollUpSpeed;
+        }
+
+        if(tenceNo == 2 && drums.volume <= 1)
+        {
+            drums.volume += Time.deltaTime * rollUpSpeed;
+        }
+        else if (tenceNo != 2 && drums.volume > 0)
+        {
+            drums.volume -= Time.deltaTime * rollUpSpeed;
+        }
+
+        if(tenceNo == 1 && coolOff)
+        {
+            coolOffTimer += Time.deltaTime;
+            if(coolOffTimer >= coolOffTime)
+            {
+                tenceNo = 0;
+                coolOffTimer = 0;
+                coolOff = false;
+            }
+        }
+    }
+
+    public void Pistol()
+    {
+        if (pistol.Length <= 0)
+        {
+            pistolExists = false;
+        }
+        else
+        {
+            pistolExists = true;
+        }
+        if (pistolExists)
+        {
+            pistolNo = Random.Range(0, pistol.Length);
+            pistol[pistolNo].Play();
+        }
+    }
+
+    public void FootSteps ()
+    {
+        if (footSteps.Length <= 0)
+        {
+            footStepExists = false;
+        }
+        else
+        {
+            footStepExists = true;
+        }
+        if (footStepExists)
+        {
+            footStepNo = Random.Range(0, footSteps.Length);
+            footSteps[footStepNo].Play();
+        }
+    }
+
+    public void Ricoshate()
+    {
+        if (ricoshate.Length <= 0)
+        {
+            ricoshateExists = false;
+        }
+        else
+        {
+            ricoshateExists = true;
+        }
+        if (ricoshateExists)
+        {
+            ricoshateNo = Random.Range(0, ricoshate.Length);
+            ricoshate[ricoshateNo].Play();
+        }
+    }
+
+    public void EnemyHurt()
+    {
+        if (enemyHurt.Length <= 0)
+        {
+            enemyHurtExists = false;
+        }
+        else
+        {
+            enemyHurtExists = true;
+        }
+        if (enemyHurtExists)
+        {
+            enemyHurtNo = Random.Range(0, enemyHurt.Length);
+            enemyHurt[enemyHurtNo].Play();
+        }
+    }
+
+    public void EnemyAttack()
+    {
+        if (enemyAttack.Length <= 0)
+        {
+            enemyAttackExists = false;
+        }
+        else
+        {
+            enemyAttackExists = true;
+        }
+        if (enemyAttackExists)
+        {
+            enemyAttackNo = Random.Range(0, enemyAttack.Length);
+            enemyAttack[enemyAttackNo].Play();
+        }
+    }
+    public void EnemyDie()
+    {
+        if (enemyDie.Length <= 0)
+        {
+            enemyDieExists = false;
+        }
+        else
+        {
+            enemyDieExists = true;
+        }
+        if (enemyDieExists)
+        {
+            enemyDieNo = Random.Range(0, enemyDie.Length);
+            enemyDie[enemyDieNo].Play();
+        }
+    }
+
+    public void EnemyCharge()
+    {
+        if (enemyCharge.Length <= 0)
+        {
+            enemyChargeExists = false;
+        }
+        else
+        {
+            enemyChargeExists = true;
+        }
+        if (enemyChargeExists)
+        {
+            enemyChargeNo = Random.Range(0, enemyCharge.Length);
+            enemyCharge[enemyChargeNo].Play();
+        }
+    }
+
+    public void PlayerHey()
+    {
+        if (playerHey.Length <= 0)
+        {
+            playerHeyExists = false;
+        }
+        else
+        {
+            playerHeyExists = true;
+        }
+        if (playerHeyExists)
+        {
+            playerHeyNo = Random.Range(0, playerHey.Length);
+            playerHey[playerHeyNo].Play();
+        }
+    }
+    public void PlayerHurt()
+    {
+        if (playerHurt.Length <= 0)
+        {
+            playerHurtExists = false;
+        }
+        else
+        {
+            playerHurtExists = true;
+        }
+        if (playerHurtExists)
+        {
+            playerHurtNo = Random.Range(0, playerHurt.Length);
+            playerHurt[playerHurtNo].Play();
+        }
+    }
+
+    public void PlayerCelebrate()
+    {
+        if (playerCelebrate.Length <= 0)
+        {
+            playerCelebrateExists = false;
+        }
+        else
+        {
+            playerCelebrateExists = true;
+        }
+        if (playerCelebrateExists)
+        {
+            playerCelebrateNo = Random.Range(0, playerCelebrate.Length);
+            playerCelebrate[playerCelebrateNo].Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Nathans Shit/PlayerMovement.cs
@@ -0,0 +1,399 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPS.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        //public Joystick joy;
+        public GameObject joyStick;
+
+        public int playerNo;
+
+        [Header("Game Mode")]
+        public controllerMode currentMode;
+
+        public bool doomControl;
+        public bool modernControl;
+        public bool mobileControl;
+        public bool mobileControl1;
+
+        [Header("Moving")]
+        public float moveSpeedDoom;
+        public float moveSpeedMordern;
+
+        //[SerializeField] float jumpSpeed;
+
+        public float walkTime;
+        float walkTimer;
+
+        public bool walking;
+
+        [Header("Turning")]
+        public bool turning;
+        public float rotateSpeed;
+        public float mouseScence;
+
+        [Header("Inputs")]
+        public float verticalInput;
+        public float horizontalInput;
+        public float verticalInput1;
+        public float horizontalInput1;
+
+        bool backWards;
+
+        [Header("Fall")]
+        public bool isGrounded;
+        public float fallRate;
+        public float fallCoolDown = 1f;
+        float fallCoolDownTimer;
+
+        [Header("Climbing")]
+        public bool climbing;
+        public bool upDownClimb;
+        public float climbSpeed;
+        public float climbCoolDown = 1f;
+        public bool canUp;
+
+        public bool strafeLeft;
+        public bool strafeRight;
+
+        public bool rotateLeft;
+        public bool rotateRight;
+
+        public enum controllerMode
+        {
+            DOOM,
+            MODERN,
+            MOBILE,
+            MOBILE1
+        }
+
+        private void Awake()
+        {
+            switch (currentMode)
+            {
+                case (controllerMode.DOOM):
+                    doomControl = true;
+                    modernControl = false;
+                    mobileControl = false;
+                    mobileControl1 = false;
+                    break;
+
+                case (controllerMode.MODERN):
+                    doomControl = false;
+                    modernControl = true;
+                    mobileControl = false;
+                    mobileControl1 = false;
+                    break;
+
+                case (controllerMode.MOBILE):
+                    doomControl = false;
+                    modernControl = false;
+                    mobileControl = true;
+                    mobileControl1 = false;
+                    break;
+
+                case (controllerMode.MOBILE1):
+                    doomControl = false;
+                    modernControl = false;
+                    mobileControl = false;
+                    mobileControl1 = true;
+                    break;
+            }
+        }
+
+        Rigidbody m_Rigidbody;
+        //GameController gameController;
+        //Audio audio;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_Rigidbody = GetComponent<Rigidbody>();
+            //gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+            //audio = GameObject.FindGameObjectWithTag("Audio").GetComponent<Audio>();
+
+            /*if (!gameController.mobileControl)
+            {
+                joyStick.SetActive(false);
+            }*/
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (!climbing)
+            {
+                Movement();
+                Rotation();
+                Fall();
+                m_Rigidbody.useGravity = true;
+            }
+            else
+            {
+                Climbing();
+            }
+
+            if(walking || turning)
+            {
+                walkTimer += Time.deltaTime;
+                if (walkTimer >= walkTime)
+                {
+                    //audio.FootSteps();
+                    walkTimer = 0;
+                }
+            }
+
+            MobileExtraButtons();
+        }
+
+        void Movement()
+        {
+            if (doomControl)
+            {
+                if(playerNo == 1)
+                {
+                    verticalInput = Input.GetAxis("Vertical");
+                }
+                else
+                {
+                    verticalInput = Input.GetAxis("P2Vertical");
+                }
+                
+
+                if (verticalInput > 0)
+                {
+                    m_Rigidbody.velocity = transform.forward * moveSpeedDoom;
+                }
+
+                if (verticalInput < 0)
+                {
+                    m_Rigidbody.velocity = -transform.forward * moveSpeedDoom;
+                    backWards = true;
+                }
+                else
+                {
+                    backWards = false;
+                }
+            }
+
+            if (modernControl)
+            {
+
+                if (playerNo == 1)
+                {
+                    verticalInput = Input.GetAxis("Vertical");
+                    horizontalInput = Input.GetAxis("Horizontal");
+                }
+                else
+                {
+                    verticalInput = Input.GetAxis("P2Vertical");
+                    horizontalInput = Input.GetAxis("P2Horizontal");
+                }
+                
+
+                if (verticalInput > 0)
+                {
+                    m_Rigidbody.AddForce(transform.forward * moveSpeedMordern);
+                }
+                if (verticalInput < 0)
+                {
+                    m_Rigidbody.AddForce(-transform.forward * moveSpeedMordern);
+                }
+                if (horizontalInput > 0)
+                {
+                    m_Rigidbody.AddForce(transform.right * moveSpeedMordern);
+                }
+                if (horizontalInput < 0)
+                {
+                    m_Rigidbody.AddForce(-transform.right * moveSpeedMordern);
+                }
+            }
+
+            
+           
+            if (verticalInput != 0 || horizontalInput != 0 || strafeLeft || strafeRight)
+            {
+                walking = true;
+            }
+            else
+            {
+                walking = false;  
+            }
+        }
+
+        void Rotation()
+        {
+            if (doomControl)
+            {
+                if (playerNo == 1)
+                {
+                    horizontalInput = Input.GetAxis("Horizontal");
+                }
+                else
+                {
+                    horizontalInput = Input.GetAxis("P2Horizontal");
+                }
+
+                if (horizontalInput1 > 0)
+                {
+                    transform.Rotate(new Vector3(0, Mathf.Lerp(0f, 1f, horizontalInput1), 0) * Time.deltaTime * rotateSpeed);
+                }
+                if (horizontalInput1 < 0)
+                {
+                    transform.Rotate(new Vector3(0, Mathf.Lerp(-1f, 0f, horizontalInput1), 0) * Time.deltaTime * rotateSpeed);
+                }
+            }
+
+            if(mobileControl)
+            {
+                //horizontalInput1 = joy.Horizontal;
+                if (horizontalInput1 > 0)
+                {
+                    transform.Rotate(new Vector3(0, horizontalInput1, 0) * Time.deltaTime * rotateSpeed);
+                }
+                if (horizontalInput1 < 0)
+                {
+                    transform.Rotate(new Vector3(0, horizontalInput1, 0) * Time.deltaTime * rotateSpeed);
+                }
+            }
+
+            if (modernControl)
+            {
+
+                if (playerNo == 1)
+                {
+                    horizontalInput1 = Input.GetAxis("Horizontal1");
+                    verticalInput1 = Input.GetAxis("Vertical1");
+                }
+                else
+                {
+                    horizontalInput1 = Input.GetAxis("P2Horizontal1");
+                    verticalInput1 = Input.GetAxis("P2Vertical1");
+                }
+               
+
+                if (horizontalInput1 > 0)
+                {
+                    transform.Rotate(new Vector3(0, horizontalInput1, 0) * Time.deltaTime * rotateSpeed);
+                }
+                if (horizontalInput1 < 0)
+                {
+                    transform.Rotate(new Vector3(0, horizontalInput1, 0) * Time.deltaTime * rotateSpeed);
+                }
+
+                if (playerNo == 1)
+                {
+                    float mousehorizontalInput1 = Input.GetAxis("Mouse X");
+                    float mouseverticalInput1 = Input.GetAxis("Mouse Y");
+                    if (mousehorizontalInput1 > 0)
+                    {
+                        transform.Rotate(Vector3.up * mouseScence);
+                    }
+                    if (mousehorizontalInput1 < 0)
+                    {
+                        transform.Rotate(Vector3.up * -mouseScence);
+                    }
+                }
+
+                
+            }
+
+            
+
+            if (horizontalInput1 != 0)
+            {
+                turning = true;
+            }
+            else
+            {
+                turning = false;
+            }
+        }
+
+
+        void MobileExtraButtons()
+        {
+            if(strafeLeft)
+            {
+                m_Rigidbody.velocity = -transform.right * moveSpeedDoom;
+            }
+
+            if (strafeRight)
+            {
+                m_Rigidbody.velocity = transform.right * moveSpeedDoom;
+            }
+
+            if(rotateLeft)
+            {
+                transform.Rotate(new Vector3(0, Mathf.Lerp(0f, -1f, 1f), 0) * Time.deltaTime * rotateSpeed);
+            }
+            if(rotateRight)
+            {
+                transform.Rotate(new Vector3(0, Mathf.Lerp(0f, 1f, 1f), 0) * Time.deltaTime * rotateSpeed);
+            }
+        }
+
+        void Climbing()
+        {
+            m_Rigidbody.useGravity = false;
+
+            if (!mobileControl)
+            {
+                verticalInput = Input.GetAxis("Vertical");
+            }
+            else
+            {
+                //verticalInput = joy.Vertical;
+            }
+
+            if (verticalInput > 0 && canUp)
+            {
+                m_Rigidbody.velocity = transform.up * climbSpeed;
+            }
+            if (verticalInput < 0)
+            {
+                m_Rigidbody.velocity = -transform.up * climbSpeed;
+            }
+
+            if (verticalInput != 0)
+            {
+                walking = true;
+            }
+            else
+            {
+                walking = false;
+            }
+        }
+
+        void Fall()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(this.transform.position, -this.transform.up, out hit, 1.1f))
+            {
+                isGrounded = true;
+            }
+            else
+            {
+                isGrounded = false;
+            }
+
+            if (!isGrounded)
+            {
+                fallCoolDownTimer += Time.deltaTime;
+                if (fallCoolDownTimer >= fallCoolDown)
+                {
+                    m_Rigidbody.AddForce(-transform.up * fallRate);
+                }
+            }
+            else
+            {
+                fallCoolDownTimer = 0;
+            }
+        }
+
+      
+    }
+}
