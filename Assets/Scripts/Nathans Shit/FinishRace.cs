6789ea8df7cd90ff99062f1e6b85a36f1d34b6dc
Smashface <51160929+Smashface90@users.noreply.github.com>
2023-02-05 12:05:29 +0800

    race stuff

--- Assets/Scripts/Nathans Shit/FinishRace.cs
+++ Assets/Scripts/Nathans Shit/FinishRace.cs
@@ -13,8 +13,21 @@ public class FinishRace : MonoBehaviour
     public float timer;
     public float time;
     public string countDown;
-
     public bool start;
+
+    //Evan's things
+    public GameObject readyTxt;
+    public GameObject num3;
+    public GameObject num2;
+    public GameObject num1;
+    public GameObject goTxt;
+
+    public GameObject winLeft;
+
+    public GameObject winRight;
+
+    //sounds
+    public AudioSource goSound;
 
     // Start is called before the first frame update
     void Start()
@@ -30,31 +43,67 @@ public class FinishRace : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!start)
+            timer -= Time.deltaTime;        
+
+        WinSeq();
+        TimerSeq();
+
+
+        if(finished)
         {
-            timer += Time.deltaTime;
-            if (timer >= 5)
+            winner = "Player " + winnerNo + " Wins!!";
+            Debug.Log(winner);
+            Time.timeScale = 0f;
+        }       
+    }
+
+public void WinSeq()
+{
+    if(winnerNo == 0)
+        {
+            winLeft.SetActive(true);
+        }
+
+        if(winnerNo == 1)
+        {
+            winRight.SetActive(true);
+        }
+}
+
+public void TimerSeq()
+{
+    if (timer <= 10)
             {
                 countDown = "Ready!";
                 Debug.Log(countDown);
             }
-            if(timer >= 8)
+            if(timer <= 8)
             {
+                num3.SetActive(true);
                 countDown = "3!";
                 Debug.Log(countDown);
             }
-            if (timer >= 10)
+            if (timer <= 6)
             {
+                num3.SetActive(false);
+                num2.SetActive(true);
                 countDown = "2!";
                 Debug.Log(countDown);
             }
-            if (timer >= 12)
+            if (timer <= 4)
             {
+                num2.SetActive(false);
+                num1.SetActive(true);
                 countDown = "1!";
-                Debug.Log(countDown);
+                Debug.Log(countDown);                
             }
-            if (timer >= 14)
+            if (timer <= 2)
             {
+                goSound.Play();
+                num1.SetActive(false);
+                readyTxt.SetActive(false);
+                goTxt.SetActive(true);
+                //playsound
                 countDown = "GO!";
 
                 Debug.Log(countDown);
@@ -62,19 +111,15 @@ public class FinishRace : MonoBehaviour
                 {
                     player[i].canMove = true;
                 }
-                start = true;
+                
             }
-        }
 
-        if(finished)
-        {
-            winner = "Player " + winnerNo + " Wins!!";
-            Debug.Log(winner);
-            Time.timeScale = 0f;
-        }
-        
-    }
-
+            if (timer <= 0)
+            {
+                goTxt.SetActive(false);
+                timer = 0;
+            }
+}
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == ("Player"))
